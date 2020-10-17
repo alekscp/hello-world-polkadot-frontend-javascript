59656a2407f393d82ede3271b0061111a1b08782
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2020-10-17 18:50:05 +0200

    [ci skip] Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://alekscp.github.io/hello-world-polkadot-frontend-javascript/precache-manifest.8a8f18bd6a000301abd0dd5b4943fc35.js"
+  "/substrate-front-end-template/precache-manifest.d6fd329f8cbc782db5f64b27ef2c2f1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://alekscp.github.io/hello-world-polkadot-frontend-javascript/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/substrate-front-end-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
